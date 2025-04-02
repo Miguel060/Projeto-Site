eb998cffaa028650f1b6986f59fe6ef3cc6124f9
Miguel <migueldoiche2@gmail.com>
2025-04-02 20:41:28 -0300

    Update project and bug fixes

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const leftForm = document.querySelector('#leftForm');
+const rightForm = document.querySelector('#rightForm');
 const iName = document.querySelector('#name')
 const iGenre = document.querySelector('#genre')
 const iTel = document.querySelector('#tel')
 const iEmail = document.querySelector('#email')
 const iPassword = document.querySelector('#password')
+const iEmailR = document.querySelector('#r_email')
+const iPasswordR = document.querySelector('#r_password')
+
 
 function limpar() {
     iName.value = ''
@@ -14,7 +18,7 @@ function limpar() {
 }
 
 function cadastrar() {
-    fetch('http://localhost:8080/inicio/cadastro', {
+    fetch('http://localhost:8080/auth/cadastrar', {
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
@@ -36,19 +40,25 @@ function cadastrar() {
         })
 }
 
-function login(){
-    fetch('http://localhost:8080/inicio/login', {
+function login() {
+    fetch('http://localhost:8080/auth/login', {
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
-        method: "GET",
+        method: "POST",
         body: JSON.stringify({
-            email: iEmail.value,
-            password: iPassword.value
+            email: iEmailR.value,
+            senha: iPasswordR.value
         })
     })
-        .then((response))
+        .then((response)=>response.text())
+        .then((response) => {
+            console.log(response)
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
 }
 
 leftForm.addEventListener('submit', function (event) {
@@ -56,4 +66,12 @@ leftForm.addEventListener('submit', function (event) {
     cadastrar()
     limpar()
 })
+
+rightForm.addEventListener('submit', function (event) {
+    event.preventDefault()
+    login()
+    limpar()
+})
+
+
 
